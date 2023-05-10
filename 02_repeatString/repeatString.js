36ab44adbf9c68fe3feda898f4a3f9d775c3fb26
Adem Hoskin <ademjhoskin@gmail.com>
2023-05-10 13:17:17 -0400

    test 2 written

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, integer) {
+    if (integer < 0) {
+        return "ERROR";
+    }
+    let newString = "";
+    for (let i = 0; i < integer; i++) {
+        newString += string;
+    }
+    return newString;
 };
 
 // Do not edit below this line
